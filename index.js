251085194dfb44760515fed49ed1817ee8c49fd5
Vasyl Vezhdel <vezhdel.vasya2001@gmail.com>
2023-04-08 15:43:45 +0300

    login, register, profile logic added

--- index.js
+++ index.js
@@ -1,34 +1,124 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import { validationResult } from 'express-validator';
 
+import { validationResult } from 'express-validator';
 import { registerValidation } from './validations/auth.js';
 
+import UserModel from './models/User.js';
+import checkAuth from './utils/checkAuth.js';
+
 mongoose
-    .connect('mongodb+srv://admin:prototype28@sushi-room.io6uttd.mongodb.net/?retryWrites=true&w=majority')
+    .connect('mongodb+srv://admin:prototype28@sushi-room.io6uttd.mongodb.net/sushi-room?retryWrites=true&w=majority')
     .then(() => console.log('Database reached successfuly'))
     .catch((err) => console.log("Error. Can't reach database", err))
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Wrong login or password.',
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if (!isValidPass) {
+            return res.status(404).json({
+                message: 'Wrong login or password.',
+            })
+        }
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, 'secretkey123', {
+            expiresIn: '14d',
+        }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed login.',
+        })
+    }
 });
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        })
+
+        const user = await doc.save();
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, 'secretkey123', {
+            expiresIn: '14d',
+        }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed registration.',
+        })
     }
 
-    res.json({
-        success: true,
-    });
 });
 
+app.get('/auth/me', checkAuth, async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId);
+        if (!user) {
+            return res.status(404).json({
+                message: 'User is not found.'
+            })
+        }
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json(userData);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'No access.',
+        })
+    }
+})
 
 app.listen(4444, (err) => {
     if (err) {
