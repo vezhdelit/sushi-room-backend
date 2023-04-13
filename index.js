e686e05b1b584b9078fa2673599fd41231b5e978
Vasyl Vezhdel <vezhdel.vasya2001@gmail.com>
2023-04-14 00:32:10 +0300

    added additional libraries

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, (err) => {
+    if (err) {
+        return console.log("Error. Can't start server", err);
+    }
+    console.log('Server started successfuly');
+});
