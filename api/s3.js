8516acf0b8b0795edb5a969f2877680b6c579e16
Vezhdel Vasyl <vezhdel.vasya2001@gmail.com>
2023-06-18 17:42:52 +0300

    implemented image uploading to side s3 server

---
+++ api/s3.js
@@ -0,0 +1,56 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { S3, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+
+const BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+const BUCKET_REGION = process.env.AWS_BUCKET_REGION;
+const ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+const SECRET_KEY = process.env.AWS_SECRET_KEY;
+
+const s3 = new S3({
+  region: BUCKET_REGION,
+  credentials: {
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_KEY,
+  },
+});
+
+export const addImage = async (req, res) => {
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: req.file.originalname,
+    Body: req.file.buffer,
+    ContentType: req.file.mimetype,
+  };
+  const command = new PutObjectCommand(params);
+
+  await s3.send(command);
+
+  res.json({
+    url: `${req.file.originalname}`,
+  });
+};
+
+export const getImage = async (req, res) => {
+  const key = req.params.key;
+
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: key,
+  };
+
+  try {
+    const { Body } = await s3.getObject(params);
+
+    // Set appropriate headers
+    res.setHeader("Content-Type", "image/jpeg");
+    res.setHeader("Content-Disposition", `attachment; filename="${key}"`);
+
+    // Pipe the S3 object directly to the response
+    Body.pipe(res);
+  } catch (error) {
+    console.error("Error downloading image:", error);
+    res.status(500).json({ error: "Failed to download image" });
+  }
+};
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import multer from 'multer';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
 
-import { registerValidation, loginValidation } from './validations/auth.js';
+import { registerValidation, loginValidation } from "./validations/auth.js";
 import {
   register,
   login,
@@ -13,101 +13,87 @@ import {
   update,
   addFavourite,
   removeFavourite,
-} from './controllers/UserController.js';
-import{uploadFile} from './controllers/UploadController.js';
+} from "./controllers/UserController.js";
 
-
-import { itemCreateValidation } from './validations/item.js';
-import { 
-  createItem, 
-  getAllItems, 
+import { itemCreateValidation } from "./validations/item.js";
+import {
+  createItem,
+  getAllItems,
   getOneItem,
   deleteItem,
-  updateItem
-} from './controllers/ItemController.js';
+  updateItem,
+} from "./controllers/ItemController.js";
 
-import { adCreateValidation } from './validations/ad.js';
-import { createAd, getAllAds } from './controllers/AdController.js';
+import { adCreateValidation } from "./validations/ad.js";
+import { createAd, getAllAds } from "./controllers/AdController.js";
 
-import handleValidationErrors from './middleware/handleValidationErrors.js';
-import checkAuth from './middleware/checkAuth.js';
+import handleValidationErrors from "./middleware/handleValidationErrors.js";
+import checkAuth from "./middleware/checkAuth.js";
+
+import { addImage, getImage } from "./api/s3.js";
 
 const app = express();
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
 app.use(cors());
 dotenv.config();
 
-const storage = multer.diskStorage({
-  destination: (_, __, cb) =>{
-    cb(null, "uploads");
-  },
-  filename: (_, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({storage});
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log('Database reached successfuly'))
+  .then(() => console.log("Database reached successfuly"))
   .catch((err) => console.log("Error. Can't reach database", err));
 
 ///////////////////////////////////////////////
 
-app.get('/', (req, res) => {
-  res.send('Server is up.');
+app.get("/", (req, res) => {
+  res.send("Server is up.");
 });
 
-////
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-
-import path from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
- //TODO: make checkAdmin
-app.post('/upload', upload.single('image'), uploadFile);
-
-app.get('/upload', (req, res) => {
-  const directoryPath = path.join(__dirname, 'uploads');
-
-  // Скануємо папку uploads та повертаємо список файлів
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      console.log('Помилка при скануванні папки:', err);
-      return res.status(500).send('Помилка сервера');
-    }
-
-    res.send(files);
-  });
-});
+//TODO: make checkAdmin
+app.post("/upload", upload.single("image"), addImage);
+app.get("/upload/:key", getImage);
 
 ////
 
-app.post('/auth/login', loginValidation, handleValidationErrors, login);
-app.post('/auth/register', registerValidation, handleValidationErrors, register);
-app.get('/auth/profile', checkAuth, profile);
-app.delete('/auth', checkAuth, remove);
-app.patch('/auth', registerValidation, handleValidationErrors, checkAuth, update);
+app.post("/auth/login", loginValidation, handleValidationErrors, login);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  register
+);
+app.get("/auth/profile", checkAuth, profile);
+app.delete("/auth", checkAuth, remove);
+app.patch(
+  "/auth",
+  registerValidation,
+  handleValidationErrors,
+  checkAuth,
+  update
+);
 
-app.patch('/auth/addFavourite', checkAuth, addFavourite);
-app.patch('/auth/removeFavourite', checkAuth, removeFavourite);
+app.patch("/auth/addFavourite", checkAuth, addFavourite);
+app.patch("/auth/removeFavourite", checkAuth, removeFavourite);
 
 ////
 
-app.post('/items', itemCreateValidation, handleValidationErrors, createItem);
-app.get('/items', getAllItems);
-app.get('/items/:id', getOneItem);
-app.delete('/items/:id', deleteItem); //TODO: make checkAdmin
-app.patch('/items/:id', itemCreateValidation, handleValidationErrors, updateItem);
+app.post("/items", itemCreateValidation, handleValidationErrors, createItem);
+app.get("/items", getAllItems);
+app.get("/items/:id", getOneItem);
+app.delete("/items/:id", deleteItem); //TODO: make checkAdmin
+app.patch(
+  "/items/:id",
+  itemCreateValidation,
+  handleValidationErrors,
+  updateItem
+);
 
 ////
 
-app.post('/ads', adCreateValidation, handleValidationErrors, createAd);
-app.get('/ads', getAllAds);
+app.post("/ads", adCreateValidation, handleValidationErrors, createAd);
+app.get("/ads", getAllAds);
 
 //////////////////////////////////////////////
 
@@ -115,5 +101,5 @@ app.listen(process.env.PORT || 5000, (err) => {
   if (err) {
     return console.log("Error. Can't start server", err);
   }
-  console.log('Server started successfuly');
+  console.log("Server started successfuly");
 });
